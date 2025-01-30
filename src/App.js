15114254756ec49e607ad5dc7c0fd451a889f2d8
Crudu Cristian <cristian.crudu@usm.md>
2025-01-30 12:16:21 +0200

    Modifica continutul App.js sa coincida cu tutorialul

--- src/App.js
+++ src/App.js
@@ -1,9 +1,3 @@
 export default function Square() {
-    return (
-        <>
-            <button className="square">X</button>
-            <button className="square">X</button>
-            <button className="square">X</button>
-        </>
-    );
+    return <button className="square">X</button>;
 }
